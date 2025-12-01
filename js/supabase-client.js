baccaa4c6aedc812c23ba7d0d2a3f592278ce33c
rula34studiend <21271034@tuxtla.tecnm.mx>
2025-12-01 14:32:47 -0600

    Return a pagina principal
    Al momento de iniciar sesion con google no redirigia a la pagina principal entonces se espera que con este pequeño cambie se logre

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -9,7 +9,7 @@ async function loginConRedSocial(provider) {
     const { data, error } = await _supabase.auth.signInWithOAuth({
         provider: provider,
         options: {
-            redirectTo:'https://obzenoproject.netlify.app/index.html'
+            redirectTo:'https://obzenoproject.netlify.app'
         }
     });
     if (error) console.error('Error social:', error);
