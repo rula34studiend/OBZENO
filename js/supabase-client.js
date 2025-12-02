4641c4a792cb364e34bd5090e7076ec8d7f48f2a
raulc <raulcristiani16@gmail.com>
2025-12-02 12:40:47 -0600

    cambio de link

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -26,7 +26,7 @@ async function cerrarSesion() {
 async function loginConRedSocial(provider) {
     const { error } = await _supabase.auth.signInWithOAuth({
         provider: provider,
-        options: { redirectTo: ' http://localhost:8888' }
+        options: { redirectTo: 'https://cdb19260--obzenoproject.netlify.live' }
     });
     if (error) console.error('Error social:', error);
 }
